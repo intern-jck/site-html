e7d919bd9d5b6fd7e5d174e23b52a0e0dacb9dc8
intern-laptop <justin.c.kirk@gmail.com>
2022-12-19 16:48:07 -0500

    adds carousel content

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,52 +2,69 @@
 // examples
 // https://codepen.io/czesster/pen/eYKqMPY
 
-
 let currentSlide = 0;
 
-const createCarousel = (slides) => {
+const createCarousel = (slides, mechDiv) => {
+  console.log(mechDiv)
 
   // Container div for carousel content
   const carouselDiv = document.createElement('div');
   carouselDiv.classList = 'carousel-div';
 
+  const carouselContent = document.createElement('div');
+  carouselContent.classList = 'carousel-content';
+
+  // Create carousel images
+  const carouselImages = [];
+
+  for (let i in slides) {
+    const carouselImageContainer = document.createElement('div');
+    carouselImageContainer.classList = 'carousel-img';
+    const carouselImage = document.createElement('img');
+
+    carouselImage.src = slides[i];
+    carouselImageContainer.appendChild(carouselImage);
+    carouselContent.appendChild(carouselImageContainer);    
+  }
+
   // Control buttons
   const leftButton = document.createElement('button');
   leftButton.classList = 'carousel-left-button';
-  leftButton.textContent = 'NEXT';
+  leftButton.textContent = 'BACK';
   leftButton.onclick = (event) => {
     if (currentSlide > 0) {
       currentSlide -= 1;
+      console.log('back:', currentSlide)
     } 
     showSlide(slides, currentSlide);
   };
 
   const rightButton = document.createElement('button');
-  rightButton.classList = 'carousel-left-button';
-  rightButton.textContent = 'BACK';
+  rightButton.classList = 'carousel-right-button';
+  rightButton.textContent = 'NEXT';
   rightButton.onclick = (event) => {
     if (currentSlide < slides.length) {
       currentSlide += 1;
+      console.log('next:', currentSlide)
     } 
     showSlide(slides, currentSlide);
   };
 
-  // Carousel image
-  const carouselImageContainer = document.createElement('div');
-  const carouselImage = document.createElement('img');
-
   // Fill Carousel
   carouselDiv.appendChild(leftButton);
-  carouselImageContainer.appendChild(carouselImage);
-  carouselDiv.appendChild(carouselImageContainer);
+  carouselDiv.appendChild(carouselContent); 
   carouselDiv.appendChild(rightButton);   
-    
+  mechDiv.appendChild(carouselDiv);
+  
 };
-
 
 const showSlide = (slides, index) => {
   slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    // slide.style.transform = `translateX(${100 * (i - index)}%)`;
   });
 };
 
+// window.onload = (event) => {
+//   createCarousel();
+//   console.log('carousel loaded')
+// };
--- scripts/mecha.js
+++ scripts/mecha.js
@@ -9,9 +9,6 @@ const clearDiv = (parent) => {
 
 const addBackButton = (backToView) => {
   // Remove it if it's there.
-
-  console.log('adding back button')
-  // removeBackButton();
   const backButton = document.createElement('button');
   backButton.textContent = 'BACK';
   backButton.setAttribute('id', 'back-button');
@@ -25,12 +22,10 @@ const addBackButton = (backToView) => {
 }
 
 const removeBackButton = () => {
-  console.log('removing back button')
   if (document.getElementById('back-button')) {
       document.getElementById('back-button').remove();
   }
 };
-
 
 const getMecha = () => {
   fetch(MECHA_URL)
@@ -49,7 +44,6 @@ const addMechaCard = (mecha) => {
   removeBackButton();
 
   mecha.forEach((mech) => {
-    // console.log(Object.keys(mech));
 
     const mechDiv = document.createElement('div');
     mechDiv.classList = "mech-div";
@@ -76,28 +70,17 @@ const addMechaCard = (mecha) => {
 };
 
 const showMechaProject = (mech) => {
+  console.log(mech)
   
   const mechaDiv = document.getElementById('mecha-projects');
   clearDiv(mechaDiv);
   addBackButton();
 
-  console.log(Object.keys(mech));
-
   const mechDiv = document.createElement('div');
   mechDiv.classList = "mech-project-div";
 
   // Carousel
-  const mechImgDiv = document.createElement('div');
-  mechImgDiv.classList = "mech-project-img";
-  const mechImg = document.createElement('img');
-  mechImg.src = mech.photos[0];
-
-  // mechImgDiv.appendChild(mechImg);
-  // mechDiv.appendChild(mechImgDiv);
-
-  mechDiv.appendChild(createCarousel(mech.photos))
-
-
+  createCarousel(mech.photos, mechDiv);
 
   // Info
   const mechInfoDiv = document.createElement('div');
@@ -105,93 +88,11 @@ const showMechaProject = (mech) => {
   const mechName = document.createElement('h2');
   mechName.classList = "mech-project-name";
   mechName.textContent = mech.name;
-  mechInfoDiv.appendChild(mechName);
-  
+  mechInfoDiv.appendChild(mechName);  
 
   mechDiv.appendChild(mechInfoDiv);
   mechaDiv.appendChild(mechDiv);
 };
-
-
-
-
-let currentSlide = 0;
-
-const createCarousel = (images) => {
-
-  // Container div for carousel content
-  const carouselDiv = document.createElement('div');
-  carouselDiv.classList = 'carousel-div';
-
-  // Carousel image  NEEDED?
-  const carouselImageContainer = document.createElement('div');
-  carouselImageContainer.classList = 'carousel-img-container';
-  // const carouselImage = document.createElement('img');
-
-  // Create a slide for each image.
-  const slides = [];
-  images.forEach((image) => {
-    console.log(image)
-    const slideDiv = document.createElement('div');
-    const slide = document.createElement('img');
-    slide.src = image;
-    slideDiv.classList = 'carousel-slide-div';
-    slideDiv.appendChild(slide);
-    carouselImageContainer.appendChild(slideDiv);
-    slides.push(slideDiv);
-  });
-
-  carouselDiv.appendChild(carouselImageContainer);  
-
-  // Control buttons
-  const leftButton = document.createElement('button');
-  leftButton.classList = 'carousel-left-button';
-  leftButton.textContent = 'NEXT';
-  leftButton.onclick = (event) => {
-    if (currentSlide > 0) {
-      currentSlide -= 1;
-    } 
-    // showSlide(slides, currentSlide);
-  };
-
-  const rightButton = document.createElement('button');
-  rightButton.classList = 'carousel-left-button';
-  rightButton.textContent = 'BACK';
-  rightButton.onclick = (event) => {
-    if (currentSlide < slides.length) {
-      currentSlide += 1;
-    }
-    // showSlide(slides, currentSlide);
-  };
-
-  // Fill Carousel
-  carouselDiv.appendChild(leftButton);
-  carouselDiv.appendChild(carouselImageContainer);
-  carouselDiv.appendChild(rightButton);
-  return carouselDiv;
-};
-
-const showSlide = (slides, index) => {
-  console.log(slides, index)
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
-  });
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.onload = (event) => {
   getMecha();
