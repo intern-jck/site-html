5d2a6a3b1f741392be37f14a32d44933c312acd8
intern-laptop <justin.c.kirk@gmail.com>
2022-12-24 09:59:53 -0500

    refactors nav bar for all screens

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-const atlastURI = 'https://data.mongodb-api.com/app/data-qbimb/endpoint/data/v1';
+// const atlastURI = 'https://data.mongodb-api.com/app/data-qbimb/endpoint/data/v1';
 
-
-const sampleRequest = `curl --request POST \
-  'https://data.mongodb-api.com/app/<Data API App ID>/endpoint/data/v1/action/findOne' \
-  --header 'Content-Type: application/json' \
-  --header 'api-key: <Data API Key>' \
-  --data-raw '{
-      "dataSource": "<cluster name>",
-      "database": "learn-data-api",
-      "collection": "people",
-      "filter": { "name": "John Sample" }
-  }'`;
+// const sampleRequest = `curl --request POST \
+//   'https://data.mongodb-api.com/app/<Data API App ID>/endpoint/data/v1/action/findOne' \
+//   --header 'Content-Type: application/json' \
+//   --header 'api-key: <Data API Key>' \
+//   --data-raw '{
+//       "dataSource": "<cluster name>",
+//       "database": "learn-data-api",
+//       "collection": "people",
+//       "filter": { "name": "John Sample" }
+//   }'`;
   
-
-
-
-  
+window.onload = (event) => {
+  console.log('main loaded')
+  // getMecha();
+  // toggleMenu();
+};
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,11 @@
 
-const toggleMenu = (event) => {
+const showMenu = (event) => {
   const navbarContent = document.getElementById('navbar-content');
-  navbarContent.classList.toggle('hide-menu');
-};
-
-window.onload = (event) => {
-  toggleMenu();
+  if (!navbarContent.style.display) {
+    navbarContent.style.display = 'block';
+  } else if (navbarContent.style.display == 'none') {
+    navbarContent.style.display = 'block';
+  } else if (navbarContent.style.display === 'block'){
+    navbarContent.style.display = 'none';
+  }
 };
