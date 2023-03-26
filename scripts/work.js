dcac29b2d67c6993d2f153f2ee9db5f862d7a9af
Justin C Kirk <justin.c.kirk@gmail.com>
2023-03-26 13:37:25 -0400

    work refactor mvp

--- scripts/work.js
+++ scripts/work.js
@@ -28,134 +28,123 @@ const removeBackButton = () => {
 
 const getWork = () => {
   fetch(WORK_JSON_URL)
-    .then((response) => (response.json()))
+    .then((response) => {
+      return response.json()
+    })
     .then((data) => {
-      console.log(data)
       addWorkCards(data.reverse());
     })
     .catch((error) => (console.log('fetching work url', error)));
 };
 
 const addWorkCards = (work) => {
-  console.log(work)
-
-  const workDiv = document.getElementById('work-container');
-  clearDiv(workDiv);
+  const workContainer = document.getElementById('work-container');
+  clearDiv(workContainer);
   removeBackButton();
-
   const workProjects = document.createElement('div');
   workProjects.classList = 'work-projects-div';
-
   work.forEach((work) => {
-
     const workCardDiv = document.createElement('div');
     workCardDiv.classList = 'work-card-div';
-
     const workName = document.createElement('h2');
     workName.classList = 'work-name';
     workName.textContent = work.name;
-
     const workImgDiv = document.createElement('div');
     workImgDiv.classList = 'work-img';
     workImgDiv.onclick = (event) => {
       event.preventDefault();
-      showMechaProject(work);
+      showWork(work);
     };
-
     const workImg = document.createElement('img');
     workImg.src = work.photos[0];
-
     workCardDiv.appendChild(workName);
     workImgDiv.appendChild(workImg);
     workCardDiv.appendChild(workImgDiv);
     workProjects.appendChild(workCardDiv);
   });
-
-  workDiv.appendChild(workProjects);
-
+  workContainer.appendChild(workProjects);
 };
 
-// const showMechaProject = (work) => {
-//   console.log(Object.keys(work))
-//   const workDiv = document.getElementById('work-container');
-//   clearDiv(workDiv);
-//   addBackButton('work-container');
+const showWork = (work) => {
+  const workContainer = document.getElementById('work-container');
+  clearDiv(workContainer);
+  addBackButton('work-container');
 
-//   const workDiv = document.createElement('div');
-//   workDiv.classList = 'work-project-div';
+  const workDiv = document.createElement('div');
+  workDiv.classList = 'work-project-div';
 
-//   // Carousel
-//   createCarousel(work.photos, workDiv);
+  // Carousel
+  createCarousel(work.photos, workDiv);
 
-//   // Info
-//   // TODO: Write function to generate info
-//   const workInfoDiv = document.createElement('div');
-//   workInfoDiv.classList = 'work-project-info';
+  // Info
+  // TODO: Write function to generate info
+  const workInfoDiv = document.createElement('div');
+  workInfoDiv.classList = 'work-project-info';
 
-//   // Add name
-//   const workName = document.createElement('div');
-//   workName.classList = 'work-info-key';
-//   workName.textContent = `NAME:`;
-//   const workNameValue = document.createElement('span');
-//   workNameValue.classList = 'work-info-value';
-//   workNameValue.textContent = `${work.name}`
-//   workName.appendChild(workNameValue);
-//   workInfoDiv.appendChild(workName);
+  // Add name
+  const workName = document.createElement('div');
+  workName.classList = 'work-info-key';
+  workName.textContent = `NAME:`;
+  const workNameValue = document.createElement('span');
+  workNameValue.classList = 'work-info-value';
+  workNameValue.textContent = `${work.name}`
+  workName.appendChild(workNameValue);
+  workInfoDiv.appendChild(workName);
 
-//   // Add client 
-//   const workClient = document.createElement('div');
-//   workClient.classList = 'work-info-key';
-//   workClient.textContent = `CLIENT:`;
-//   const workClientValue = document.createElement('a');
-//   workClientValue.href = work.client_url;
-//   workClientValue.target = '_blank';
-//   workClientValue.classList = 'work-info-value';
-//   workClientValue.textContent = `${work.client}`
-//   workClient.appendChild(workClientValue);
-//   workInfoDiv.appendChild(workClient);
+  // Add client 
+  const workClient = document.createElement('div');
+  workClient.classList = 'work-info-key';
+  workClient.textContent = `CLIENT:`;
+  const workClientValue = document.createElement('a');
+  workClientValue.href = work.client_url;
+  workClientValue.target = '_blank';
+  workClientValue.classList = 'work-info-value';
+  workClientValue.textContent = `${work.client}`
+  workClient.appendChild(workClientValue);
+  workInfoDiv.appendChild(workClient);
 
-//   // Add date
-//   const workDate = document.createElement('div');
-//   workDate.classList = 'work-info-key';
-//   workDate.textContent = `DATE:`;
-//   const workDateValue = document.createElement('span');
-//   workDateValue.classList = 'work-info-value';
-//   workDateValue.textContent = `${work.date}`
-//   workDate.appendChild(workDateValue);
-//   workInfoDiv.appendChild(workDate);
+  // Add date
+  const workDate = document.createElement('div');
+  workDate.classList = 'work-info-key';
+  workDate.textContent = `DATE:`;
+  const workDateValue = document.createElement('span');
+  workDateValue.classList = 'work-info-value';
+  workDateValue.textContent = `${work.date}`
+  workDate.appendChild(workDateValue);
+  workInfoDiv.appendChild(workDate);
 
-//   // Add tech
-//   const workTech = document.createElement('div');
-//   workTech.classList = 'work-info-key';
-//   workTech.textContent = `TECH:`;
+  // Add tech
+  const workTech = document.createElement('div');
+  workTech.classList = 'work-info-key';
+  workTech.textContent = `TECH:`;
 
-//   const workTechValue = document.createElement('div');
-//   workTechValue.classList = 'work-info-tech';
+  const workTechValue = document.createElement('div');
+  workTechValue.classList = 'work-info-tech';
 
-//   for (let i in work.tech) {
-//     const techTag = document.createElement('a');
-//     techTag.textContent = work.tech[i][0];
-//     techTag.href = work.tech[i][1];
-//     techTag.target = '_blank';
-//     workTechValue.appendChild(techTag);
-//   }
+  for (let i in work.tech) {
+    const techTag = document.createElement('a');
+    techTag.textContent = work.tech[i][0];
+    techTag.href = work.tech[i][1];
+    techTag.target = '_blank';
+    workTechValue.appendChild(techTag);
+  }
 
-//   workTech.appendChild(workTechValue);
-//   workInfoDiv.appendChild(workTech);
+  workTech.appendChild(workTechValue);
+  workInfoDiv.appendChild(workTech);
 
-//   // Add info
-//   const workInfo = document.createElement('div');
-//   workInfo.classList = 'work-info-key';
-//   workInfo.textContent = `INFO:`;
-//   const workInfoValue = document.createElement('span');
-//   workInfoValue.classList = 'work-info-value';
-//   workInfoValue.textContent = `${work.info}`
-//   workInfo.appendChild(workInfoValue);
-//   workInfoDiv.appendChild(workInfo);
+  // Add info
+  const workInfo = document.createElement('div');
+  workInfo.classList = 'work-info-key';
+  workInfo.textContent = `INFO:`;
+  const workInfoValue = document.createElement('span');
+  workInfoValue.classList = 'work-info-value';
+  workInfoValue.textContent = `${work.info}`
+  workInfo.appendChild(workInfoValue);
+  workInfoDiv.appendChild(workInfo);
 
-//   workDiv.appendChild(workInfoDiv);
-//   workDiv.appendChild(workDiv);
-// };
+  workDiv.appendChild(workInfoDiv);
+  workContainer.appendChild(workDiv);
+};
 
 window.addEventListener("load", function (event) {
   getWork();
