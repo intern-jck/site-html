350ff8fc70fe7611ff232b6cba15637951f2b262
Justin Kirk <justin.c.kirk@gmail.com>
2025-02-16 14:51:50 -0500

    nav mvp

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,4 @@ navToggle.addEventListener("click", () => {
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
-//   primaryHeader.toggleAttribute("data-overlay");
 });
