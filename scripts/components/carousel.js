1d764f32738bc2eed863c63842954a95806b3e3a
Justin Kirk <justin.c.kirk@gmail.com>
2025-02-26 14:12:18 -0500

    removes some debt

--- scripts/components/carousel.js
+++ scripts/components/carousel.js
@@ -9,26 +9,6 @@ class Carousel {
         this.test = "test";
 
         this.create();
-        // Init observer
-        // this.element = document.getElementById(`${this.name}-list`);
-        // this.options = {
-        //     root: this.element,
-        //     threshold: 0.85,
-        // };
-
-        // this.observer = new IntersectionObserver(this.handleIntersection.bind(this), this.options);
-
-        // const divs = document.querySelectorAll(`.${this.name}-slide`);
-        // console.log(divs)
-        // for (let i = 0; i <= nodes.length; i++) {
-        //     const div = document.querySelector(`#${this.name}-slide-${i}`)
-        //     console.log(div)
-        //     this.observer.observe(div);
-        // }
-        // for (let div in divs) {
-            // console.log(div)
-            // this.observer.observe(div);
-        // }
     }
 
     prevSlide() {
@@ -124,43 +104,4 @@ class Carousel {
         carouselContainer.append(carouselDots);
     }
 
-    handleIntersection(entries, observer) {
-        console.log(entries);
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                // const nextSibling = entry.target.nextElementSibling;
-                // const prevSibling = entry.target.previousElementSibling;
-
-                // let prevId = "";
-                // let nextId = "";
-
-                // if (!prevSibling) {
-                //     prevId = observer.root.lastElementChild.id;
-                // } else {
-                //     prevId = prevSibling.id;
-                // }
-
-                // if (!nextSibling) {
-                //     nextId = observer.root.firstElementChild.id;
-                // } else {
-                //     nextId = nextSibling.id;
-                // }
-
-                const targetId = entry.target.id;
-
-                this.slideNumber = parseInt(targetId.split("-")[targetId.length - 1]);
-                console.log(targetId);
-            }
-        });
-    }
-
-    // initObserver() {
-    //     // this.slideCount = this.nodes.length;
-    //     // this.slideNumber = 1;
-
-    //     const divs = document.querySelectorAll(`${this.name}-slide`);
-    //     for (let div of divs) {
-    //         this.observer.observe(div);
-    //     }
-    // }
 }
